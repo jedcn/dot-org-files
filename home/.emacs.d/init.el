1ba0c1cd9bfbb6688047b79a04460943a63dffb9
Jed Northridge <git@jedcn.com>
2015-05-10 16:02:50 -0400

    [emacs] See matches while searching

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -168,3 +168,8 @@
 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\M-P" 'execute-extended-command)
+
+(define-key isearch-mode-map (kbd "C-o")
+  (lambda () (interactive)
+    (let ((case-fold-search isearch-case-fold-search))
+      (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
