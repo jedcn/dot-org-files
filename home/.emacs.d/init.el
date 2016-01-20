77a3871ad0395e561c1dd933eac599c88c9b3629
Jed Northridge <git@jedcn.com>
2016-01-19 22:44:16 -0500

    [web-mode] Setup basic hook w/ indentation

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -58,6 +58,13 @@
 
 (setq js-indent-level 2)
 
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (add-hook 'after-init-hook 'global-company-mode)
