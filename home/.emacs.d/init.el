71d368c61223542363e803a43b696affde05057a
Jed Northridge <git@jedcn.com>
2016-04-20 17:14:05 -0400

    [emacs] Bring in org-bullets. Setup bullet lists.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -56,6 +56,12 @@
 
 (setq org-hide-emphasis-markers t)
 
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(font-lock-add-keywords 'org-mode
+                        '(("^ +\\([*]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
 (setq coffee-tab-width 2)
 
 (setq js-indent-level 2)
