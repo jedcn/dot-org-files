87b430b0db14275d27a814abcfce3ce20ca10455
Jed Northridge <git@jedcn.com>
2017-03-21 22:13:10 -0400

    [emacs] Get rid of "nice" headings in org-mode

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -60,23 +60,6 @@
                         '(("^ +\\([*]\\) "
                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-(let* ((variable-tuple (cond ((x-list-fonts "Lucida Grande") '(:font "Lucida Grande"))
-                             ((x-list-fonts "Verdana")       '(:font "Verdana"))
-                             (nil (warn "Cannot find Lucida Grande or Verdana."))))
-       (base-font-color     (face-foreground 'default nil 'default))
-       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-
-  (custom-theme-set-faces 'user
-                          `(org-level-8 ((t (,@headline ,@variable-tuple))))
-                          `(org-level-7 ((t (,@headline ,@variable-tuple))))
-                          `(org-level-6 ((t (,@headline ,@variable-tuple))))
-                          `(org-level-5 ((t (,@headline ,@variable-tuple))))
-                          `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-                          `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-                          `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-                          `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-                          `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
-
 (setq coffee-tab-width 2)
 
 (setq js-indent-level 2)
